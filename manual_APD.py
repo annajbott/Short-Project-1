ae997cbf840ab4e40c03f12b38f456a95da07312
Anna James-Bott <annott@clpc418.cs.ox.ac.uk>
2018-04-30 11:26:09 +0100

    Chaste code incorporated. Works for high pacing

--- manual_APD.py
+++ manual_APD.py
@@ -6,95 +6,186 @@ import numpy as np
 
 def ap_duration(d, paces, repolarisation = 90 , time_interval = None):
 
-    # Convert membrane potential and time arrays to numpy
+    # Convert membrane potential and time lists to numpy arrays
     V = np.asarray(d['membrane.V'])
+    # Depending on model, sometimes environment.time, others engine.time
     time = np.asarray(d['environment.time'])
 
-    # Numpy arrays containing start of APs and duration of APs
-    # Assuming no 1:2 (or 1:3 etc) ratios, so number of paces >= number of APs
-    start_ap = np.zeros(paces + 2) # Plus 2 in case starts mid AP and ends with incomplete AP
-    start_indexes = np.zeros(paces + 2)
+    # Blank numpy arrays to contain resting values, max peaks for each AP
+    # Times of peaks, duration of APs and start of AP above some threshold
+    # Assuming no 1:2 (or 1:3 etc.) ratios, so number of paces >= number of APs
+    resting_values = np.zeros(paces + 2)
+    peak_values = np.zeros(paces + 2)
+    time_of_peak_values = np.zeros(paces + 2)
+    onset = np.zeros(paces + 2) # Plus 2 in case starts mid AP and ends with incomplete AP
     duration_ap = np.zeros(paces + 2)
-    end_ap = np.zeros(paces + 2)
-    end_indexes = np.zeros(paces + 2)
 
-    # Segmenting each period using -75mV as AP threshold.
-    AP_threshold = -80
+    # Segmenting each period using the minimum voltage + 5mV as threshold
+    AP_threshold = min(V) + 5
+    # Boolean whether in AP or outside
     in_ap = 0
+    # Counter for number of APs
     responses = 0
-    for i in range(0,len(V)):
-        # Start of AP (so not already in AP), greater than threshold
-        if V[i] >= AP_threshold and not in_ap:
+    max_upstroke_velocity = 0.0 # Unsure if needed for this purpose
+    inf = float("inf")
+
+    current_minimum_velocity = inf
+    current_resting_value = inf
+    current_peak = -inf
+    resting_potential_gradient_thresh = 0.01 # Gradient taken from Chaste code
+
+    # Boolean for if section of AP where gradient < 0.01 has been found
+    flat_bit_found = 0
+    apd_end_time = None
+    apd_start_time = None
+
+    # Iterating over time points
+    for i in range(1,len(V)):
+        # Taken from Chaste code: using gradients to determine upstrokes and rest
+
+        # Gradient calculated using difference in potential over time difference from previous time point
+        voltage_grad = (V[i]-V[i-1])/float((time[i]-time[i-1]))
+        if voltage_grad >= max_upstroke_velocity:
+            max_upstroke_velocity = voltage_grad
+            current_time_upstroke = time[i]
+
+        # Looking for rest section of AP, either flat-ish gradient or minimum voltage
+        if abs(voltage_grad) <= current_minimum_velocity and abs(voltage_grad) <= resting_potential_gradient_thresh and not in_ap:
+            current_minimum_velocity = abs(voltage_grad)
+            current_resting_value = V[i-1]
+            flat_bit_found  = 1
+
+        # Keep storing mimimum voltage if flat bit has not been found for period
+        elif V[i-1] < current_resting_value and flat_bit_found == 0 and not in_ap:
+            current_resting_value = V[i-1]
+
+        ## Crossing threshold. Start of AP ##
+        if V[i] > AP_threshold and V[i-1] <= AP_threshold and not in_ap:
             in_ap = 1
-            start_ap[responses] = time[i]
-            start_indexes[responses] = i
-        # End of AP, record end time and subtract start for the duration
-        elif in_ap and V[i] < AP_threshold:
+
+            # Set resting value for this AP. Used as minimum
+            resting_values[responses] = current_resting_value
+
+            # Re-initialise these values for next AP
+            current_minimum_velocity = inf
+            current_resting_value = inf
+            flat_bit_found = 0
+
+            # Linear interpolation to store onset time.
+            # y = y0 + (x -x0)*(y1-y0)/(x1 -x0). y: time, x : voltage
+            onset[responses] = time[i-1] + (AP_threshold - V[i-1])*(time[i]-time[i-1])/(V[i]-V[i-1])
+
+            switching_phase = True
+
+        # In AP, record height of the peak and corresponding time. Useful check if printed
+        elif in_ap and V[i] > current_peak:
+            current_peak = V[i]
+            current_peak_time = time[i]
+
+        #if prev_voltage_grad <= 0 and voltage_grad > 0 and not switching_phase:
+
+
+        #switching_phase = False
+
+        # Cross threshold again. End of AP. Update variables
+        elif in_ap and V[i] < AP_threshold and V[i-1] >= AP_threshold:
+
+            # No longer in AP
             in_ap = 0
-            end_ap[responses] = time[i]
-            end_indexes[responses] = i
-            duration_ap[responses] = end_ap[responses] - start_ap[responses]
+
+            # Record peak values
+            peak_values[responses] = current_peak
+            time_of_peak_values[responses] = current_peak_time
+
+            # Updates tally of number of APs
             responses += 1
 
-    # If simulation was started in the middle of an AP, discard first entry
-    if V[0] > AP_threshold:
-        start_ap = start_ap[1:]
-        duration_ap = duration_ap[1:]
-    # If final AP not finished, discard its start time
-    if V[-1] > AP_threshold:
-        start_ap = start_ap[0:-1]
+            # Re-initialise values for next AP
+            current_peak_time = -inf
+            max_upstroke_velocity = -inf
+            current_peak = AP_threshold
+            current_time_of_upstroke_velocity = 0.0
+
+        # Record previous voltage
+        prev_voltage_grad = voltage_grad
+
+        # Loop round to next time point
 
     # Keep only non-zero terms. Matrices containing start and duration of APs at set threshold
-    start_ap = start_ap[np.nonzero(start_ap)]
-    end_ap = end_ap[np.nonzero(end_ap)]
-    duration_ap = duration_ap[np.nonzero(duration_ap)]
+    onset = onset[np.nonzero(onset)]
+    peak_values = peak_values[np.nonzero(peak_values)]
+    time_of_peak_values = time_of_peak_values[np.nonzero(time_of_peak_values)]
 
     ### Varying thresholds calculated for each AP ###
     ### ----------------------------------------- ###
-    # 2nd part if want the thresholds to vary for the size of the AP
+    # Using min and max of each AP, calculating specific 90% of AP duration
 
-    # Clone matrices from earlier section
-    duration2_ap = duration_ap
-    start2_ap = start_ap
+    # Clone onset matrix, to fill with APD (90/50 etc) starts
+    onset_apd = onset
 
-    for i in range(0,len(duration2_ap)):
-        #print i
-        if i == 0:
-            # For first action potential
-            ap_period_start = 0
-            ap_period_end = int(start_indexes[i+1])
+    # Iterating over APs rather than time points
+    for ap_index in range(0, len(peak_values)):
+        # Custom threshold for each AP. Range*(100-repolarisation %) + resting value
+        custom_thresh = resting_values[ap_index] + 0.01*(100-repolarisation)*(peak_values[ap_index]-resting_values[ap_index])
 
-        elif i == len(start_ap) - 1:
-            # For middle APs
-            ap_period_start = int(end_indexes[i-1])
-            ap_period_end = None
+        starting_time_index = np.nonzero(time > onset[ap_index])[0][0] - 1
+        #print starting_time_index
 
+        # The APD 90/50.. threshold is greater than earlier threshold
+        # Look forwards in time
+        if custom_thresh >= AP_threshold:
+            prev_v = V[starting_time_index]
+            prev_t = time[starting_time_index]
+            for t in range(starting_time_index,len(time)):
+                if V[t] > custom_thresh and V[t-1] < custom_thresh:
+                    # Linear interpolation
+                    apd_start_time = prev_t + ((custom_thresh - prev_v) / float(V[t] - prev_v)) * (time[t] - prev_t)
+                    onset_apd[ap_index] = apd_start_time
+                    apd_starting_index = t
+                    break
+                prev_v = V[t]
+                prev_t = time[t]
+
+        # Look backwards to find custom threshold, i.e. threshold is lower than previous threshold used
         else:
-            # For final action potential
-            ap_period_start = int(end_indexes[i-1]) + 1
-            ap_period_end  = int(start_indexes[i+1]) -1
+            prev_v = V[starting_time_index + 1]
+            prev_t = time[starting_time_index + 1]
+            for t in range(starting_time_index, 0, -1):
+                if V[t] < custom_thresh:
+                    # Linear interpolation
+                    apd_start_time = prev_t + ((custom_thresh - prev_v) / float((V[t] - prev_v))) * (time[t] - prev_t)
+                    onset_apd[ap_index] = apd_start_time
+                    apd_starting_index = (t + 1);
+                    break;
+                prev_v = V[t]
+                prev_t = time[t]
 
-        ap_period = V[ap_period_start:ap_period_end]
-
-        # Calculate range and threhsold for each individual AP
-        custom_range = (max(ap_period) - min(ap_period))
-        custom_thresh = min(ap_period) + custom_range*(100-repolarisation)*0.01
-
-        # Cut off the front end and back end of each interval, ensure not exceeding threshold already.
-        while ap_period[0] >= custom_thresh:
-            ap_period = ap_period[1:]
-
-        while ap_period[-1] >= custom_thresh:
-            ap_period = ap_period[:-1]
-
-        # Find the first and last instance where threshold is exceeded.
-        start_index = np.nonzero(ap_period> custom_thresh)[0][0] + ap_period_start
-        end_index = np.nonzero(ap_period> custom_thresh)[0][-1] + ap_period_start
-        duration2_ap[i] = time[end_index] - time[start_index]
-        start2_ap[i] = time[start_index]
+        if apd_end_time != None and apd_start_time != None and apd_start_time < apd_end_time:
+            # Skip to next AP if start of this AP is reached before last AP is finished
+            continue
 
 
-    return[start2_ap, duration2_ap]
+        # If apd_start_index has a value then look forwards in time for repolarisation
+        if apd_starting_index != None:
+            prev_t = time[apd_starting_index - 1]
+            prev_v = V[apd_starting_index -1]
+            for t in range(apd_starting_index, len(time)):
+                if V[t] < custom_thresh:
+                    # Linear interpolation
+                    apd_end_time = time[t - 1] + ((custom_thresh - prev_v) / (V[t] - prev_v)) * (time[t] - time[t - 1])
+                    duration_ap[ap_index] = apd_end_time - apd_start_time
+                    break
+                prev_t = time[t]
+                prev_v = V[t]
+
+        # Re-initialise for next AP.
+        apd_starting_index = None
+        apd_end_time = None
+        apd_start_time = None
+        # Loop to next AP
+
+    duration_ap = duration_ap[np.nonzero(duration_ap)]
+    return[onset_apd, duration_ap]
 
 def steady(m,p,pcl,paces):
     # Create simulation using user defined model and protocol
@@ -109,8 +200,8 @@ def steady(m,p,pcl,paces):
     # Lengthy step due to log_interval component, so progress report when this is finished
     print "Simulations finished running"
 
-    # Use function to calculate start and AP duration, threshold -75mV
-    start, duration = ap_duration(d, paces, threshold = -75)
+    # Use function to calculate start and AP duration, default APD 90
+    start, duration = ap_duration(d, paces)
 
 
     # If there is greater than a 1:1 ratio
@@ -176,8 +267,9 @@ def steady(m,p,pcl,paces):
     return(ss)
 
 def main():
-    #Set pacing
-    bcl = 80
+    ## Initialisation common to steady and APD calc ##
+    # Set pacing
+    bcl = 100
 
     # Load model and set protocol, create simulation
     m = myokit.load_model('ohara-cipa-v1-2017.mmt')
@@ -189,26 +281,26 @@ def main():
     cell_type =  'Epicardial'
     s.set_constant('cell.celltype', cell_types[cell_type])
 
-    # Pre-pace simulation to steady state and run 5 cycles of simulation
-    s.pre(400*bcl)
 
-    # Set threshold, 90% of repolarisation to resting membrane potential
-    # thresh = 0.9 * s.state()[m.get('membrane.V').indice()]
-
-    paces = 30
-    #ss = steady(m,p, bcl, paces = 1000)
-    #print ss
-    #s = myokit.Simulation(m,p)
-    d = s.run(paces*bcl)#, log_interval = 0.1 )
-
-
+    ### Testing APD calc section ###
+    ### ------------------------ ###
+    # Pre-pace simulation and run 30 cycles of simulation
+    s.pre(200*bcl)
     # Running using function
-    start, duration = ap_duration(d, paces = 30)
+    paces = 10
+    d = s.run(paces*bcl)
+    start, duration = ap_duration(d, paces)
     print start
     print duration
     pl.figure()
     pl.plot(d['environment.time'], d['membrane.V'])
-    pl.show()
 
+    '''
+    ### Testing steady state calc ###
+    ### ------------------------- ###
+    ss = steady(m,p, bcl, paces = 300)
+    print ss
+    '''
+    pl.show()
 if __name__ == "__main__":
     main()
