a60f8486d931b094f2882f5d5778fba106bcc488
Anna James-Bott <annott@clpc418.cs.ox.ac.uk>
2018-05-04 15:57:42 +0100

    HF toggle added

--- Ohara-restitution.py
+++ Ohara-restitution.py
@@ -3,14 +3,17 @@ import myokit
 import matplotlib.pyplot as pl
 from manual_APD import ap_duration
 import numpy as np
-
+from HF_model import Ord_HF_Gomez
+'''
 ## O'hara Restitution
 ## S1S2 steady state S1 pacing at 1000ms, single
 
 pcl = 1000
 step_size = 50
 m = myokit.load_model('ohara-2011.mmt')
-
+HF = True
+if HF:
+    m = Ord_HF_Gomez()
 p = myokit.pacing.blocktrain(pcl, 0.5, offset=0, level=1.0, limit=0)
 s = myokit.Simulation(m, p)
 
@@ -33,7 +36,7 @@ for percent in [90, 70, 50, 30]:
         p.schedule(1, 4*pcl + pacing, 0.5, pcl, 1)
         s.set_protocol(p)
         d = s.run(pacing + 10*pcl)
-        start, duration = ap_duration(d, 5, repolarisation = percent)
+        start, duration, thresh = ap_duration(d, 5, repolarisation = percent)
 
         apd_duration.append(duration[-1])
         if percent == 90:
@@ -47,7 +50,7 @@ for percent in [90, 70, 50, 30]:
 # Plot S1S2 protocol restitution curve
 pl.xlabel('Diastole interval (ms)')
 pl.ylabel('APD (ms)')
-pl.title('Ohara {} Cell Steady State S1-S2 Protocol Restitution Curve (Fig 5B)'.format(cell_type))
+pl.title('Ohara {} Cell Steady State S1-S2 Protocol Restitution Curve'.format(cell_type))
 pl.legend(['APD 90','APD 70','APD 50','APD 30'])
 pl.xlim(10,10000)
 pl.xscale('log')
@@ -56,6 +59,13 @@ pl.show()
 
 ### Dynamic Protocol ###
 ### ---------------- ###
+m = myokit.load_model('ohara-2011.mmt')
+HF = True
+if HF:
+    m = Ord_HF_Gomez()
+# Set cell type
+cell_types = {'Endocardial': 0, 'Epicardial' : 1, 'Mid-myocardial' : 2}
+cell_type = 'Endocardial'
 
 # Empty arrays to fill.
 period = []
@@ -84,7 +94,7 @@ s.set_constant('cell.mode', cell_types[cell_type])
 d = s.run(offset, log = ['membrane.V','engine.time'])
 
 # Use ap_duration function to calculate start times and durations
-start, duration = ap_duration(d, 30000*len(pacing_list), repolarisation = 95)
+start, duration, thresh = ap_duration(d, 30000*len(pacing_list), repolarisation = 95)
 
 # First offset equal to zero, so remove first entry from the list
 offset_list = offset_list[1:]
@@ -92,7 +102,11 @@ print offset_list
 # Numpy array to contain final APD for each pacing cycle
 final_apd = np.zeros(len(offset_list) + 1)
 final_apd2 = np.zeros(len(offset_list) + 1)
+final_apd3 = np.zeros(len(offset_list) + 1)
+final_apd4 = np.zeros(len(offset_list) + 1)
 di2 = []
+di3 = []
+di4 = []
 
 # Fill final_apd array
 for i in range(len(offset_list)):
@@ -100,15 +114,16 @@ for i in range(len(offset_list)):
 
     # Index_start = The index of the start of pacing cycle in start array
     index_start = np.nonzero(np.round(start,0) >= offset_list[i])[0][0]
-    print "start", start[index_start], "duration", duration[index_start]
-    print "start -1 ", start[index_start -1], "duration", duration[index_start-1]
-    print "period", period[i], "di", period[i]-duration[index_start -2]
 
     # index_1 -1 to get peak at the end of the previous cycle
     final_apd[i] = duration[index_start]
     final_apd2[i] = duration[index_start - 1]
+    final_apd3[i] = duration[index_start - 2]
+    final_apd4[i] = duration[index_start - 3]
     di.append(period[i]-duration[index_start -1])
     di2.append(period[i]-duration[index_start -2])
+    di3.append(period[i]-duration[index_start -3])
+    di4.append(period[i]-duration[index_start -4])
 
 # The final peak doesn't have a peak after it, so can be indexed by the final duration recorded
 final_apd[-1] = duration[-1]
@@ -117,6 +132,11 @@ di.append(period[-1] - duration[-2])
 # If the graph has a long-short pattern, take previous APD and DI as well
 final_apd2[-1] = duration[-2]
 di2.append(period[-1] - duration[-3])
+final_apd3[-1] = duration[-2]
+di3.append(period[-1] - duration[-3])
+final_apd4[-1] = duration[-2]
+di4.append(period[-1] - duration[-3])
+
 
 pl.figure()
 pl.plot(d['engine.time'],d['membrane.V'])
@@ -125,10 +145,10 @@ pl.plot(d['engine.time'],d['membrane.V'])
 pl.figure()
 pl.plot(di, final_apd, 'x', c = 'b')
 pl.plot(di2,final_apd2,'x', c = 'b')
+pl.plot(di3,final_apd3,'x', c = 'b')
+pl.plot(di4,final_apd4,'x', c = 'b')
 pl.xlabel('DI (ms)')
-pl.xlim(0,360)
-pl.ylim(190,270)
+
 pl.ylabel('APD 95 (ms)')
 pl.title('Dynamic Restitution Curve- Endothelial Cells (Ohara 2011)')
 pl.show()
-'''
