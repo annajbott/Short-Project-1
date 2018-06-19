ab83351d944079e74a972f314899963a977834c4
Anna James-Bott <annajames-bott@Annas-MacBook-Air.local>
2018-06-19 15:00:26 +0100

    trying to merge

--- grandi_s1s2.py
+++ grandi_s1s2.py
@@ -3,7 +3,8 @@ import myokit
 import matplotlib.pyplot as pl
 from manual_APD import ap_duration
 from restitution_protocols import *
-
-
 model = 'grandi-2010'
 s1s2_protocol(model, number_S1, PCL_S1 = 1000, pre_pacing = 200, min_di = 10, max_di = 1000, number_di = 30, repolarisation = 90, cell_type = 0, log_scale = False, HF_model = None, plot_from_function = True)
+
+
+# run
