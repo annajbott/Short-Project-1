524b1e945041834dd80bec327072b6b4091af5e1
Anna James-Bott <annott@clpc418.cs.ox.ac.uk>
2018-06-19 13:40:54 +0100

    Uses dynamic function instead of manually

--- tentusscher-dynamic.py
+++ tentusscher-dynamic.py
@@ -1,61 +1,47 @@
 #!/usr/bin/env python
-
 import myokit
 import matplotlib.pyplot as pl
-from manual_APD import ap_duration
+import numpy as np
+from HF_model import *
+from restitution_protocols import *
 
 ### Ten-Tusscher Dynamic Restitution Protocol- Figure 5F/J ###
 ### ------------------------------------------------------ ###
 # Starting from low pacing and increasing frequency step wise
-# Measuring the final AP for each pacing
+# Measuring the final few AP for each pacing
 
 # Get the model
 m = myokit.load_model('tentusscher-2006.mmt')
+model = 'tentusscher-2006'
+#dynamic_protocol(model, number_stages = 80, max_pcl = 1000, min_pcl = 100, voltage_plot = True, cell_type = 0, HF_model = 'Lu', log_scale = False)
 
-step_size = -30
-apd_duration = []
-alternate_apd = []
-#di = []
-period = []
-pcl_list  = []
+#model = 'ohara-2011'
 
-pcl = 800
-p = myokit.pacing.blocktrain(pcl, 0.5, offset=0, level=1.0, limit=400)
-s = myokit.Simulation(m, p,apd_var='membrane.V')
-s.pre(pcl * 400)
-s.reset()
-offset = 0
-while pcl > 100:
-    print pcl
-    #Initialising protocol with new pacing
-    '''
-    p.schedule(1, offset, 0.5, pcl, 50 )
+#dynamic_protocol(model, number_stages = 150, time_per_stage = 20000, max_pcl = 1000, min_pcl = 100, HF_model = 'Lu' ,voltage_plot = False, cell_type = 0, log_scale = False)
 
-    offset += 50*pcl
-    pcl += step_size
-    '''
-    p = myokit.pacing.blocktrain(pcl, 0.5, offset=0, level=1.0, limit=0)
-    s = myokit.Simulation(m, p,apd_var='membrane.V')
+#model = 'ohara-cipa-v1-2017'
+model = 'grandi-2010'
+dynamic_protocol(model, number_stages = 60, max_pcl = 600, min_pcl = 100, HF_model = None ,voltage_plot = False, cell_type = 0, log_scale = False)
 
-    #Pre pacing for 48 beats, running 10 beats
-    s.pre(pcl * 40)
-    thresh = 0.9 * s.state()[m.get('membrane.V').indice()]
-    d = s.run(10*pcl)
-    start, duration = ap_duration(d, 10, thresh)
-
-    apd_duration.append(duration[-1])
-    alternate_apd.append(duration[-2])
-    period.append(pcl)
-    pcl += step_size
-    s.reset()
-
-# Plotting APD vs Period for dynamic protocol
+#dynamic_protocol(model, number_stages = 60, stimuli_per_pace = 50, min_pcl = 100, voltage_plot = True, cell_type = 0)
+'''
+di_list, apd_list, max_grad = s1s2_protocol(model, number_S1 = 10, PCL_S1 = 1000, pre_pacing = 200, min_di = 10, max_di = 1000, number_di = 60, repolarisation = 90, cell_type = 0, log_scale = False, HF_model = None, plot_from_function = False)
 pl.figure()
-pl.xlabel('Period (ms)')
-pl.ylabel('APD 90 (ms)')
-pl.title('Ten-Tusscher (2006)- Dynamic restitution curve (Fig. 5J)')
-pl.xlim(0,800)
+pl.plot(di_list, apd_list, '.')
 
-pl.plot(period, apd_duration, 'x')
-pl.plot(period, alternate_apd, 'x')
+print max_grad
+
+di_list_gomez, apd_list_gomez, hf_g_max_grad = s1s2_protocol(model, number_S1 = 10, PCL_S1 = 1000, pre_pacing = 200, min_di = 10, max_di = 1000, number_di = 60, repolarisation = 90, cell_type = 0, log_scale = False, HF_model = 'Gomez', plot_from_function = False)
+di_list_elshrif, apd_list_elshrif, hf_e_max_grad = s1s2_protocol(model, number_S1 = 10, PCL_S1 = 1000, pre_pacing = 200, min_di = 10, max_di = 1000, number_di = 60, repolarisation = 90, cell_type = 0, log_scale = False, HF_model = 'Elshrif', plot_from_function = False)
+
+pl.plot(di_list_gomez, apd_list_gomez, '.')
+print hf_g_max_grad
+pl.plot(di_list_elshrif, apd_list_elshrif, '.' )
+print hf_e_max_grad
+pl.legend(['Healthy Ord', 'HF Gomez', 'HF Elshrif'])
+
+pl.ylabel('APD 90 (ms)')
+pl.xlabel('DI (ms)')
+pl.title('Ord Endocardial Cell S1-S2 Restitution Curve. 1000ms PCL')
 pl.show()
+'''
